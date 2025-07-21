4a35b19600ff0c4411c34885991e14373204cebc
Maxim <87935999+realmizzer@users.noreply.github.com>
2025-07-21 07:26:38 +0700

    upd: added myself :)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={inter.className} style={{ margin: 0 }}>
         <ConfigProvider theme={theme}>{children}</ConfigProvider>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,46 @@
 'use client';
 
+import Image from 'next/image';
 import { Provider } from 'react-redux';
-import { Layout, Typography } from 'antd';
-import { NewsList } from '@/widgets/news-feed';
+import { Flex, Layout, Typography } from 'antd';
 import { store } from '@/shared/config/store';
+import AuthorImage from '@/shared/assets/images/author.jpeg';
+import { NewsList } from '@/widgets/news-feed';
 
 const { Header, Content } = Layout;
-const { Title } = Typography;
+const { Title, Text } = Typography;
 
 export default function Home() {
   return (
     <Provider store={store}>
-      <Layout style={{ minHeight: '100vh' }}>
-        <Header>
-          <Title style={{ color: 'white', margin: 0 }}>News Feed</Title>
+      <Layout style={{ padding: '16px 180px', minHeight: '100vh' }}>
+        <Header
+          style={{
+            padding: 0,
+            marginBottom: 16,
+            backgroundColor: 'transparent',
+          }}
+        >
+          <Flex justify={'space-between'} align={'center'}>
+            <Flex align={'center'}>
+              <Title style={{ margin: 0 }}>Newsfeed</Title>
+            </Flex>
+            <Flex gap={8} align={'center'}>
+              <Image
+                src={AuthorImage.src}
+                alt={'Author'}
+                width={32}
+                height={32}
+                style={{
+                  borderRadius: '50%',
+                  objectFit: 'cover',
+                }}
+              />
+              <Text type={'secondary'}>by Maxim Nikolaev</Text>
+            </Flex>
+          </Flex>
         </Header>
-        <Content style={{ padding: '16px 128px' }}>
+        <Content>
           <NewsList />
         </Content>
       </Layout>
